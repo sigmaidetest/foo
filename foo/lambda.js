da5e30cbd051c8a7feb2257563331ea7e82a5ef0
sigmaidetest <35022586+sigmaidetest@users.noreply.github.com>
2018-01-31 06:33:29 +0530

    dyna

--- foo/lambda.js
+++ foo/lambda.js
@@ -1,5 +1,18 @@
 let AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
 
-exports.handler = function(event, context, callback) {
-	callback(null,'Successfully executed');
+exports.handler = function (event, context, callback) {
+	ddb.put({
+		TableName: 'fad',
+		Item: {
+			'k': 'b'
+		}
+	}, function (err, data) {
+		if (err) {
+			//handle error
+		} else {
+			//your logic goes here
+		}
+	});
+	callback(null, 'Successfully executed');
 }
